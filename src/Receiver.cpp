97900ab0ebd373d23f496d4a853a690f5982be85
Ahmed Miljau <ahmed.miljau@gmail.com>
2019-07-18 03:05:18 +0500

    Add Receiver

---
+++ src/Receiver.cpp
@@ -0,0 +1,103 @@
+#include "Receiver.hpp"
+
+Receiver::Receiver(MicroBit *mbit) {
+  this->mbit = mbit;
+  this->cipher = new XXTEACipher(CIPHER_KEY, CIPHER_KEY_LENGTH);
+  mbit->io.P1.eventOn(MICROBIT_PIN_EVENT_ON_PULSE);
+  DEBUG(mbit, "Receiver has been initialized");
+};
+
+Receiver::~Receiver() { delete cipher; };
+
+void Receiver::setupListeners() {
+  DEBUG(mbit, "Setting up listeners");
+  mbit->messageBus.listen(MICROBIT_ID_IO_P0, MICROBIT_PIN_EVT_PULSE_HI, this,
+                          &Receiver::onPulseHigh,
+                          MESSAGE_BUS_LISTENER_IMMEDIATE);
+  mbit->messageBus.listen(MICROBIT_ID_IO_P0, MICROBIT_PIN_EVT_PULSE_LO, this,
+                          &Receiver::onPulseLow,
+                          MESSAGE_BUS_LISTENER_IMMEDIATE);
+};
+
+void Receiver::tearDownListeners() {
+  mbit->messageBus.ignore(MICROBIT_ID_IO_P0, MICROBIT_PIN_EVT_PULSE_HI, this,
+                          &Receiver::onPulseHigh);
+  mbit->messageBus.ignore(MICROBIT_ID_IO_P0, MICROBIT_PIN_EVT_PULSE_LO, this,
+                          &Receiver::onPulseLow);
+};
+
+void Receiver::writeBit(uint8_t bit) {
+  // Refuse to write anything if we are reading the header.
+  if (bits_recieved <= 3) {
+    return;
+  };
+
+  // If all 8bytes were written, ignore it.
+  if (bits_written >= 64) {
+    return;
+  };
+  buffer[bits_written / 64] <<= (bits_recieved - 3) % 8;
+  buffer[bits_written / 64] += (bit & 0x1);
+  bits_written++;
+
+  if (bits_written >= 64) {
+    onPacket();
+  }
+};
+
+void Receiver::reset() {
+  bits_recieved = 0;
+  bits_written = 0;
+  mid_packet = 0;
+  last_hi_tick = 0;
+  // Reset the buffer
+  std::memset(buffer, 0, sizeof(buffer));
+}
+
+void Receiver::onPacket() {
+  DEBUG(mbit, "Complete!");
+  reset();
+}
+
+void Receiver::onPulseHigh(MicroBitEvent event) {
+  if (!this->mid_packet) {
+    // This means it's the header.
+    DEBUG(mbit, "Got a HI! Packet is starting");
+
+    // Check if header is over
+    if (++this->bits_recieved >= 3) {
+      mid_packet = true;
+    }
+    return;
+  }
+
+  // We are in mid packet.
+  // TODO: repetitions can overflow
+  uint8_t repetitions = event.timestamp / TX_SLEEP;
+  DEBUG(mbit, "Got a HI with %d repetitions", repetitions);
+  for (uint8_t i = 0; i < repetitions; i++) {
+    writeBit(1);
+  }
+}
+
+void Receiver::onPulseLow(MicroBitEvent event) {
+  if (!this->mid_packet) {
+    // A low pulse cannot start the listen.
+    DEBUG(mbit, "Got a LO to start the packet. Ignoring");
+    return;
+  }
+
+  // We are in mid packet.
+  // TODO: repetitions can overflow
+  uint8_t repetitions = event.timestamp / TX_SLEEP;
+  DEBUG(mbit, "Got a LO with %d repetitions", repetitions);
+  for (uint8_t i = 0; i < repetitions; i++) {
+    writeBit(0);
+  }
+}
+
+void Receiver::start() {
+  while (1) {
+    mbit->sleep(100);
+  }
+}
---
+++ src/Receiver.hpp
@@ -0,0 +1,42 @@
+#ifndef RECEIVER_H
+#define RECEIVER_H
+#include "XXTEACipher.hpp"
+#include "common.hpp"
+
+class Receiver {
+private:
+  uint8_t bits_recieved = 0;
+  uint8_t bits_written = 0;
+  bool mid_packet = 0;
+
+  // Last hi tick is used to invalidate the data
+  uint64_t last_hi_tick = 0;
+
+  // The pointer to the microbit
+  MicroBit *mbit;
+
+  uint8_t buffer[PACKET_SIZE] = {0};
+
+  // Initialize a cipher instance to encrypt and decrypt the read data
+  XXTEACipher *cipher;
+
+  // Listener setup and destruction will be taken care of by these two methods
+  void setupListeners();
+  void tearDownListeners();
+
+  // Pulse events
+  void onPulseHigh(MicroBitEvent event);
+  void onPulseLow(MicroBitEvent event);
+
+  void writeBit(uint8_t bit);
+  void onPacket();
+  void reset();
+
+public:
+  // Constructor
+  Receiver(MicroBit *mbit);
+  virtual ~Receiver();
+  void start();
+};
+
+#endif /* RECEIVER_H */
