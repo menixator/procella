01b214c7e600909673fbf0ef41ab467c58fed69c
Ahmed Miljau <ahmed.miljau@gmail.com>
2019-07-19 20:07:30 +0500

    Update flash script for two microbits

--- flash.sh
+++ flash.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 
-DRIVE=$(readlink --canonicalize /dev/disk/by-label/MICROBIT)
+MY_MICROBIT=$(readlink --canonicalize /dev/disk/by-id/usb-MBED_VFS_9900000041504e4500584001000000370000000097969901-0:0)
+ALSAN_MICROBIT=$(readlink --canonicalize /dev/disk/by-id/usb-MBED_VFS_9900000050294e45004620110000001d0000000097969901-0:0)
 
-udisksctl mount -b $DRIVE
+udisksctl mount -b $MY_MICROBIT
+udisksctl mount -b $ALSAN_MICROBIT
 
 cp build/bbc-microbit-classic-gcc/src/*-combined.hex /run/media/$(whoami)/MICROBIT/
+cp build/bbc-microbit-classic-gcc/src/*-combined.hex /run/media/$(whoami)/MICROBIT1/
