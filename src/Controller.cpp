3024c68031f579cb9866081522a83338c3a0cc1c
Ahmed Miljau <ahmed.miljau@gmail.com>
2019-07-21 05:36:27 +0500

    Documentation: Controller

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -1,11 +1,19 @@
 #include "Controller.h"
 
+/*
+ * Listener for Button A press
+ */
 void Controller::onButtonAPress(MicroBitEvent event) {
+  (void)event;
   mbit->display.animateAsync(TX_ANIM, 300, 5);
   onRoleDecided(Alpha);
 };
 
+/*
+ * Listener for Button B press
+ */
 void Controller::onButtonBPress(MicroBitEvent event) {
+  (void)event;
   mbit->display.animateAsync(RX_ANIM, 300, 5);
   onRoleDecided(Beta);
 };
@@ -24,10 +32,14 @@ void Controller::onRoleDecided(Role newRole) {
 }
 
 Controller::Controller() {
+  // Initialize the required components. ESPECIALLY THE SCHEDULER.
   mbit->init();
   setupListeners();
 };
 
+/*
+ * Sets up the basic listeners required for the Controller component
+ */
 void Controller::setupListeners() {
   mbit->messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, this,
                           &Controller::onButtonAPress);
@@ -35,6 +47,9 @@ void Controller::setupListeners() {
                           &Controller::onButtonBPress);
 };
 
+/*
+ * Removes the basic listeners required for the component.
+ */
 void Controller::tearDownListeners() {
   // If the role has been decided, assume that the listeners are already
   // removed.
@@ -45,23 +60,34 @@ void Controller::tearDownListeners() {
                           &Controller::onButtonAPress);
   mbit->messageBus.ignore(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, this,
                           &Controller::onButtonBPress);
-}
+};
 
+/*
+ * The constructor. Takes no arguments.
+ */
 Controller::~Controller() {
   tearDownListeners();
   delete mbit;
 };
 
+/*
+ * The main activity of the Controller is done here
+ */
 void Controller::start() {
   DEBUG(mbit, "Controller is starting");
+
+  // Sleep while the role is undecided.
   while (role == None) {
     mbit->sleep(ROLE_CHECK_INTERVAL);
   }
+
+  // Start the designated role controllers when the role is decided.
   if (role == Alpha) {
     DEBUG(mbit, "Initialized as a sender!");
     Sender *sender = new Sender(mbit);
     DEBUG(mbit, "Sender has been intialized");
     sender->start();
+
   } else if (role == Beta) {
     DEBUG(mbit, "Initialized as a reciever!");
     Receiver *reciever = new Receiver(mbit);
--- src/Controller.h
+++ src/Controller.h
@@ -6,28 +6,50 @@
 #include "Sender.hpp"
 #include "common.hpp"
 
-// How long to sleep when role is undecided
+/**
+ * Decides how long the Controller component will put the microbit into a sleep
+ * when the role is undecided.
+ */
 #define ROLE_CHECK_INTERVAL 15
 
 class Controller {
 private:
-  // helps differentiate between senders and recievers.
+  /**
+   * Role enumeration that is used to differentiate whether or not the microbit
+   * the program is running is is a Sender, or a Reciever.
+   */
   enum Role { None, Alpha, Beta };
 
+  /*
+   * The main MicroBit instance. Notice how it's a pointer. This is to prevent
+   * the object from being paged out.
+   */
   MicroBit *mbit = new MicroBit();
 
   // Initialize role as none.
   Role role = None;
 
 public:
+  /**
+   * Event listeners for Button A and Button B's press events.
+   */
   void onButtonAPress(MicroBitEvent event);
   void onButtonBPress(MicroBitEvent event);
   void onRoleDecided(Role role);
 
+  /**
+   * Helpers that setup listeners and tears them down when required.
+   */
   void setupListeners();
   void tearDownListeners();
 
+  /**
+   * The Constructor of the class.
+   */
   Controller();
+  /**
+   * Destructor of the class.
+   */
   virtual ~Controller();
   void start();
 };
