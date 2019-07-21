05926e4b63906570702871077243b4d0314cefd8
Ahmed Miljau <ahmed.miljau@gmail.com>
2019-07-21 05:38:41 +0500

    Documentation: main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include "Controller.hpp"
 #include "MicroBit.h"
 #include "common.hpp"
-//
-// Initialize the wrapper
-int main() {
 
+int main() {
+  // Initializes the controller.
+  // Notice how the Controller instance is being declared on the heap as opposed
+  // to the stack. The MicroBit will page out the data otherwise.
   Controller *controller = new Controller();
 
+  // Start the Controller's main loop
   controller->start();
+
+  // Free the occupied memory
+  delete controller;
 
   release_fiber();
 };
