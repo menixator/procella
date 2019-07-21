c142823d1485d4e2197907fc22e4177fc55e2751
Ahmed Miljau <ahmed.miljau@gmail.com>
2019-07-21 07:27:53 +0500

    Documentation: Receiver

--- src/Receiver.hpp
+++ src/Receiver.hpp
@@ -7,21 +7,39 @@
 
 class Receiver {
 private:
+  /**
+   * Number of bits read
+   */
   uint8_t bitsRead = 0;
 
-  // Last hi tick is used to invalidate the data
+  /**
+   * When the last activity was noticed on the GPIO pin.
+   */
   uint64_t lastActivity = 0;
+
+  /**
+   * When the last screen activity went down. Used to clean up after the screen.
+   */
   uint64_t lastScreenActivity = 0;
 
-  // The pointer to the microbit
+  /**
+   * The pointer to the microbit instance
+   */
   MicroBit *mbit;
 
+  /**
+   * Internal buffer
+   */
   uint8_t buffer[PACKET_SIZE] = {0};
 
-  // Initialize a cipher instance to encrypt and decrypt the read data
+  /**
+   *  The cipher instance to decrypt the data
+   */
   XXTEACipher *cipher;
 
-  // Listener setup and destruction will be taken care of by these two methods
+  /**
+   * Listener setup and destruction will be taken care of by these two methods
+   */
   void setupListeners();
   void tearDownListeners();
 
@@ -29,8 +47,19 @@ private:
   void onPulseHigh(MicroBitEvent event);
   void onPulseLow(MicroBitEvent event);
 
+  /*
+   * Called whenever a bit is received.
+   */
   void onBit(uint8_t bit);
+
+  /**
+   * Called whenever a full packet is recieved.
+   */
   void onPacket();
+
+  /**
+   * Resets the interal state and prepares to recieve the next packet.
+   */
   void reset();
 
 public:
